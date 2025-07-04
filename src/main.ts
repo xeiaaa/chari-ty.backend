ac5e848489b41bb08d878c902b3489d8fdf86a7e
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2025-07-04 12:31:24 +0800

    nestjs setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
 }
+
 bootstrap();
